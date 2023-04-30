eb490b4a63ce111c89a584e9bad3d078a0579502
Joe Molina <joedodaniljr123@gmail.com>
2023-04-30 13:30:30 -0400

    se me habia olvidado usar git ajua

---
+++ php/estructuraBasica.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        :root {
+            --fontColor: #fff
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index-style.css">
+    <link rel="stylesheet" href="../css/estructuraBasica.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;500;700&display=swap" rel="stylesheet">
+    <title>Joe.Dev</title>
+</head>
+<body>
+    <div class="fondo"></div>
+    <header>
+        <span>Joe.Dev</span>
+        <nav>
+            <a href="#">Inicio</a>
+            <a href="#">Proyectos</a>
+            <a href="#">Habilidades</a>
+            <a href="#">Contacto</a>
+        </nav>
+    </header>
